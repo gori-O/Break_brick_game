19919c4ecabd281dd90f8fa5ffc3d0cde12a61f3
gori-O <fheld1545@naver.com>
2023-06-19 20:43:00 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace brakewall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pboxball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("굴림", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblScore.Location = new System.Drawing.Point(12, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(137, 28);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "score : 0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(507, 584);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 40);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pboxball
+            // 
+            this.pboxball.BackColor = System.Drawing.Color.Yellow;
+            this.pboxball.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pboxball.Location = new System.Drawing.Point(283, 291);
+            this.pboxball.Name = "pboxball";
+            this.pboxball.Size = new System.Drawing.Size(40, 40);
+            this.pboxball.TabIndex = 2;
+            this.pboxball.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 33;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(457, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 32);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Game Over";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(481, 291);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 76);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "..";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1131, 636);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pboxball);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
+            this.Text = "미니게임 :: 벽돌깨기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pboxball;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace brakewall
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft;
+        bool goRight;
+        bool isGameover;
+
+        PictureBox[] blockArray;
+
+        int score;
+        int ballx;
+        int bally;
+        int playerSpeed;
+
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            setupGame();
+        }
+        private void setupGame()
+        {
+            timer1.Enabled = true;
+            label1.Visible = false;
+            label2.Visible = false;
+            score = 0;
+            ballx = 5;
+            bally = 5;
+            playerSpeed = 12;
+            lblScore.Text = "Score : " + score;
+            pboxball.Left = 200;
+            pboxball.Top = 200;
+            pictureBox1.Left = 350;
+            PlaceBlocks();
+
+            // 컨트롤 컴포넌트 들을 전부 불러오고, 그게 블럭이면 랜덤값을 이용해 다양한 색깔을 입힘.
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "blocks")
+                {
+                    x.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+                }
+            }
+
+        }
+        private void removeBlocks()
+        {
+            foreach(PictureBox x in blockArray)
+            {
+                this.Controls.Remove(x);
+            }
+        }
+        private void PlaceBlocks()
+        {
+            blockArray = new PictureBox[12];
+            int a = 0;
+            int top = 50;
+            int left = 20;
+
+            for (int i = 0; i < blockArray.Length; i++) 
+            {
+                blockArray[i] = new PictureBox();
+                blockArray[i].Height = 32;
+                blockArray[i].Width = 100;
+                blockArray[i].Tag = "blocks";
+                blockArray[i].BackColor = Color.White;
+                if(a==6)
+                {
+                    top = top + 50;
+                    left = 20;
+                    a = 0;
+                }
+                if (a < 6)
+                {
+                    a++;
+                    blockArray[i].Left = left;
+                    blockArray[i].Top = top;
+                    this.Controls.Add(blockArray[i]);
+                    left = left + 130;
+                }
+            }
+        }
+        private void GameOver(string masseage)
+        {
+            isGameover= true;
+            timer1.Stop();
+            lblScore.Text = "Score : " + score;
+            label1.Visible= true; 
+            label2.Visible= true;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblScore.Text = "Score : " + score;
+            if (goLeft == true && pictureBox1.Left > 0)
+            {
+                pictureBox1.Left -= playerSpeed;
+            }
+            if (goRight == true && pictureBox1.Right < 790)
+            {
+                pictureBox1.Left += playerSpeed;
+            }
+            pboxball.Left += ballx;
+            pboxball.Top += bally;
+            if (pboxball.Left < 0 || pboxball.Left > 770)
+            {
+                ballx = -ballx;
+            }
+            if (pboxball.Top < 0)
+            {
+                bally = -bally;
+            }
+            if (pboxball.Bounds.IntersectsWith(pictureBox1.Bounds))
+            {
+                bally = rand.Next(5, 12) * -1;
+                if (ballx < 0)
+                {
+                    ballx = rand.Next(5, 12) - 1;
+                }
+                else
+                {
+                    ballx = rand.Next(5, 12);
+                }
+            }
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "blocks")
+                {
+                    if(pboxball.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        score += 1;
+                        bally = -bally;
+                        this.Controls.Remove(x);
+                    }
+                }
+            }
+            if (score >= 12)
+            {
+                label1.Text = "You Win!!";
+                label2.Text = "enter : new game\r\nq : exit game";
+                GameOver("You Win!");
+            }
+            if (pboxball.Top > 390)
+            {
+                label1.Text = "    GameOver";
+                label2.Text = "enter : new game\r\nq : exit game";
+                GameOver("Lose :: ball out");
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+
+            }
+            else if(e.KeyCode ==Keys.Right)
+            {
+                goRight = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Enter && isGameover == true)
+            {
+                removeBlocks();
+                setupGame();
+            }
+            if (e.KeyCode == Keys.Q && isGameover == true)
+            {
+                Application.Exit();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
